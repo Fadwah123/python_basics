abec2d00057cefe6c70f2d0a5c01b71d98ad86da
Fadwah <124136101+Fadwah123@users.noreply.github.com>
2025-07-05 21:13:03 +0530

    Update and rename notes.md to foundation.py

---
+++ Day-02/foundation.py
@@ -0,0 +1,9 @@
+num=int(input("Enter a number to check if it is positive or negative:"))
+if(num<0):
+    print("The number is negative")
+elif(num==0):
+    print("The number is zero")
+else:
+    print("The number is positive")
+    
+
