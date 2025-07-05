a40342e04f5c5978e423810beddbf4a7a494c698
Fadwah <124136101+Fadwah123@users.noreply.github.com>
2025-07-05 21:42:26 +0530

    Create stretch_task.py

---
+++ Day-03/stretch_task.py
@@ -0,0 +1,10 @@
+code=" "
+while code!="python":
+    code=input("Enter secret code:")
+    if code=="python":
+        print("Access granted!")
+    else:
+        print("Wrong code!Try again")
+    
+    
+    
