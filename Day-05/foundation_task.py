6b038eb75d7127178b6f25d94092aac810737232
Fadwah <124136101+Fadwah123@users.noreply.github.com>
2025-07-16 23:13:17 +0530

    Update foundation_task.py

--- Day-05/foundation_task.py
+++ Day-05/foundation_task.py
@@ -1,4 +1,6 @@
-animal={"dog":"bark","cat":"meow","cow":"moo","duck":"quack","lion":"roar"}
-print("Animal sounds:")
-for i,j in animal.items():
-    print(i,"says",j)
+print("My favourite foods:")
+food=["Pizza","Chocolate","Sushi","Ice cream","Pasta"]
+j=1
+for i in food:
+    print(f"{j}.{i}")
+    j+=1
