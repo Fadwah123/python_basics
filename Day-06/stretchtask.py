d7632c242af822624a713ea290d4323cdecf94ec
Fadwah <124136101+Fadwah123@users.noreply.github.com>
2025-07-16 23:13:55 +0530

    Create stretchtask.py

---
+++ Day-06/stretchtask.py
@@ -0,0 +1,6 @@
+fruit={"apple":5,"banana":3}
+print(f"Original dictionary:{fruit}")
+fruit["orange"]=8
+print(f"After adding orange:{fruit}")
+fruit["apple"]=10
+print(f"After updating apple:{fruit}")
