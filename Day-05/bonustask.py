e89247e5b60a69615b483b054aa407fee6ec2651
Fadwah <124136101+Fadwah123@users.noreply.github.com>
2025-07-16 23:37:16 +0530

    Update bonustask.py

--- Day-05/bonustask.py
+++ Day-05/bonustask.py
@@ -1,67 +1,35 @@
-print("Contact Book")
-book={
-    "Alice Johnson":
-    {"Phone":"555-0123",
-     "Email":"alice@email.com",
-     "Address":"123 Main St"
-    },
-    "Bob Smith":
-    {"Phone":"555-0456",
-     "Email":"bob@email.com",
-     "Address":"425 Oak Ave"
-    }
-}
-print("1.Display all contacts.")
-print("2.Search by name.")
-print("3.Edit contact details.")
-print("4.Exit")
-
+print("Top 5 movies")
+movie=[]
+print("1.Enter 1 to add movies.")
+print("2.Enter 2 to display movies based on rating.")
+print("3.Enter 3 to display highest and lowest rated movies.")
+print("4. Exit")
 
 while True:
-    choice=int(input("Enter choice:"))
-    if choice==2:
-        search=input("Enter name to search if in the phone book:")
-        flag=False
-
-        for i,j in book.items():
-            if search.lower()==i.lower():
-                for details,value in j.items():
-                    print(f"{details}:{value}")
-                    flag=True
-                    
-        if flag==False:
-            print("Contact not found!")
-    elif choice==1:
-        for name in sorted(book):
-            print(f"{name}:")
-            for feild,value in book[name].items():
-                print(f"{feild}:{value}")
-        
-
-    elif choice == 3:
-        search = input("Enter name to edit: ")
-        found = False
-        for name in book:
-            if search.lower() == name.lower():
-                print(f"\nEditing contact: {name}")
-                for field in book[name]:
-                    current_value = book[name][field]
-                    new_value = input(f"{field} [{current_value}]: ")
-                    if new_value.strip():
-                        book[name][field] = new_value
-                print("Contact updated.")
-                found = True
-                break
-        if not found:
-            print("Contact not found.")              
-
+    choice=int(input("Enter your choice:"))
+    if choice==1:
+        if len(movie)>=5:
+            print("You can only store 5 movies!")
+            
+        else:
+            name=input("Enter the movie name:")
+            rate=int(input("Enter the movie rating(1-10):"))
+            movie.append([name,rate])
+            print("Movie added!")
+            
+    elif choice==2:
+        if not movie:
+            print("Movies list empty.")
+        else:
+            sorted_movie=sorted(movie,key=lambda x:x[1],reverse=True)
+            print(sorted_movie)
+    elif choice==3:
+            high=max(movie,key=lambda x:x[1])
+            low=min(movie,key=lambda x:x[1])
+            print("The top rated movie is:",high[0])
+            print("The least rated movie is:",low[0])
     elif choice==4:
-        print("Exiting")
+        print("Exiting!")
         break
     else:
-        print("Invalid choice!")
-        
-        
-
-
-
+        print("Invalid choice!,please enter a valid choice!")
