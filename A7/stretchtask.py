e971dd791e9faf22832c217029a70fcf16958f21
Fadwah <124136101+Fadwah123@users.noreply.github.com>
2025-07-17 00:42:59 +0530

    Update stretchtask.py

--- A7/stretchtask.py
+++ A7/stretchtask.py
@@ -1,1 +1,10 @@
+def peri(l,b):
+    return (l+b)*2
 
+def area(l,b):
+    return l*b
+print("Rectangle calculator")
+len=int(input("Length:"))
+wid=int(input("Width:"))
+print(f"Perimeter:{peri(len,wid)}")
+print(f"Area:{area(len,wid)}")
